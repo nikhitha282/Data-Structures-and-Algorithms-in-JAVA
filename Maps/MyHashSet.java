60232a0387754cfd201368debfb42605a32f192e
Konakalla Sai Nikhitha <47154427+nikhitha282@users.noreply.github.com>
2021-10-23 00:46:33 +0530

    Update MyHashSet.java
    Simplified the code by changing data type of hash set to boolean

--- Maps/MyHashSet.java
+++ Maps/MyHashSet.java
@@ -2,25 +2,21 @@
 //Constraints: 0 <= key <= 10^6
 
 class MyHashSet {
-    private int[] hashSet;
+    private boolean hashSet;
     
     public MyHashSet() {
         hashSet = new int[1000001];
-        Arrays.fill(hashSet, -1); //-1 to indicate no value at the index
     }
     
     public void add(int key) {
-        hashSet[key] = key;
+        hashSet[key] = true;
     }
     
     public void remove(int key) {
-        if(contains(key))
-            hashSet[key] = -1;
+        hashSet[key] = false;
     }
     
     public boolean contains(int key) {
-        if(hashSet[key] != -1)
-            return true;
-        return false;
+        return hashSet[key];
     }
 }
